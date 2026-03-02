ca645f34244c3828403c472909404586c498380b
JelR <resosemitojelani@gmail.com>
2026-03-02 02:37:02 -0300

    basic concept for controller and services

---
+++ backend/api/controller/userController.js
@@ -0,0 +1,13 @@
+import * as userService from "#services/fetchUserInfo.js";
+
+export const getProfile = async (req, res) => {
+  try {
+    const user = await userService.getUserById(req.params.id);
+
+    if (!user) return res.status(404).json({ message: "User not found" });
+
+    res.json(user);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
