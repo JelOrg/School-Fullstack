64cc3fb57f7cc51c1de7eede1eb233cec95ee090
JelR <resosemitojelani@gmail.com>
2026-03-02 23:45:09 -0300

    working getting data from the backend

--- backend/api/services/fetchUserInfo.js
+++ backend/api/services/fetchUserInfo.js
@@ -1,10 +1,23 @@
+// file has to do with getting all types of info that is related to users
 import { prisma } from "#utils/prismaClient";
 
-async function main() {
+async function exampleFunction(userId) {
   // Example: Fetch all records from a table
   // Replace 'user' with your actual model name
-  const allUsers = await prisma.users.findMany();
+  const allUsers = await prisma.users.findUnique({
+    where: {
+      userId: userId,
+    },
+    select: {
+      userId: true,
+      firstName: true,
+      lastName: true,
+      department: {
+        select: {
+          departmentName: true,
+        },
+      },
+    },
+  });
   console.log("All users:", JSON.stringify(allUsers, null, 2));
 }
-
-main();
